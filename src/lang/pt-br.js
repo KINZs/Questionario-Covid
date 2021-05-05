040d2241fdba8fd9dfa4fa2764fe68e360bf98c4
silvavn <37382997+silvavn@users.noreply.github.com>
2021-05-04 21:26:26 -0600

    renaming questions and cleaning file

--- src/lang/pt-br.js
+++ src/lang/pt-br.js
@@ -6,7 +6,7 @@ export default {
   
     questions:{
       /* COPIE E COLE O TEMPLATE ABAIXO PARA CRIAR NãoVAS QUESTOES */
-      QUESTAO_1:{
+      q_travel:{
         type:"radio", //NAO MUDE
         title:"Nos últimos 14 dias, o aluno/criança ou qualquer pessoa com quem viva viajou para fora do Estado?",
         subtitle:"Se for isento de exigência de quarentena (por exemplo, trabalhador de serviços essenciais que viaja regularmente a trabalho), selecione «Não».",
@@ -16,14 +16,14 @@ export default {
         ],
         next:{
             "yes":"ISOLATE",
-            "no":"QUESTAO_2",
+            "no":"q_doctor_eval",
         }
       },
       /* O TEMPLATE TERMINA AQUI */
   
   
   /* COPIE E COLE O TEMPLATE ABAIXO PARA CRIAR NãoVAS QUESTOES */
-      QUESTAO_2:{
+      q_doctor_eval:{
         type:"radio", //NAO MUDE
         title:"Um médico, prestador de serviços de saúde ou unidade de saúde pública a que o aluno/criança deveria estar em isolamento (permanecer em casa)?",
         subtitle:"Isso pode se dever a um surto ou rastreamento de contactos.",
@@ -36,7 +36,7 @@ export default {
   
   
   /* COPIE E COLE O TEMPLATE ABAIXO PARA CRIAR NãoVAS QUESTOES */
-      QUESTAO_3:{
+      q_close_contact:{
         type:"radio", //NAO MUDE
         title:"Nos últimos 14 dias, o aluno/criança foi identificado como «contacto próximo» de alguém que atualmente tem COVID-19?",
         qoptions:[ //NAO MUDE
@@ -48,7 +48,7 @@ export default {
   
   
   /* COPIE E COLE O TEMPLATE ABAIXO PARA CRIAR NãoVAS QUESTOES */
-      QUESTAO_4:{
+      q_alert:{
         type:"radio", //NAO MUDE
         title:"Nos últimos 14 dias, o aluno/criança recebeu uma Notificação de Alerta de Exposição à COVID no telemóvel?",
         subtitle:"Se já fez um teste e o resultado foi negativo, seleciona “Não.”",
@@ -59,24 +59,11 @@ export default {
       },
       /* O TEMPLATE TERMINA AQUI */
   
-  
+
   /* COPIE E COLE O TEMPLATE ABAIXO PARA CRIAR NãoVAS QUESTOES */
-      QUESTAO_5:{
+      q_fever:{
         type:"radio", //NAO MUDE
-        title:"O aluno/criança tem algum destes sintomas?",
-        subtitle:"Escolha qualquer um/todos os que sejam novos, que estejam a piorar e que não estejam relacionados com outras causas ou problemas de saúde conhecidos preexistentes.",
-       /* qoptions:[ //NAO MUDE
-          {text:"Sim", value:"yes"},
-          {text:"Não", value:"no"},
-        ]  
-       */
-      },
-      /* O TEMPLATE TERMINA AQUI */
-  
-  /* COPIE E COLE O TEMPLATE ABAIXO PARA CRIAR NãoVAS QUESTOES */
-      QUESTAO_5_A:{
-        type:"radio", //NAO MUDE
-        title:"Febre e/ou calafrios",
+        title:"O aluno/criança tem Febre e/ou calafrios",
         subtitle:"Temperatura de 37,8 graus Celsius/100 graus Fahrenheit, ou mais altas",
         qoptions:[ //NAO MUDE
           {text:"Sim", value:"yes"},
@@ -87,9 +74,9 @@ export default {
   
   
   /* COPIE E COLE O TEMPLATE ABAIXO PARA CRIAR NãoVAS QUESTOES */
-      QUESTAO_5_B:{
+      q_cough:{
         type:"radio", //NAO MUDE
-        title:"Tosse ou difteria (crupe)",
+        title:"O aluno/criança tem tosse ou difteria (crupe)",
         subtitle:"Contínuo, mais do que o habitual, com um ruído sibilante ao respirar, (não relacionado a asma, vias aéreas reativas pós-infecciosas outras causas ou problemas de saúde conhecidos preexistentes)",
         qoptions:[ //NAO MUDE
           {text:"Sim", value:"yes"},
@@ -100,10 +87,10 @@ export default {
   
   
   /* COPIE E COLE O TEMPLATE ABAIXO PARA CRIAR NãoVAS QUESTOES */
-      QUESTAO_5_C:{
+      q_breath:{
         type:"radio", //NAO MUDE
-        title:"Perda de fôlego",
-        subtitle:"Sem fôlego, incapaz de respirar profundamente, (não relacionada a asma, ou outras causas ou problemas de saúde conhecidos preexistentes)",
+        title:"O aluno/criança tem dificuldade para respirar?",
+        subtitle:"Sem fôlego, incapaz de respirar fundo, (não relacionada a asma, ou outras causas ou problemas de saúde conhecidos preexistentes)",
         qoptions:[ //NAO MUDE
           {text:"Sim", value:"yes"},
           {text:"Não", value:"no"},
@@ -112,9 +99,9 @@ export default {
       /* O TEMPLATE TERMINA AQUI */
   
   /* COPIE E COLE O TEMPLATE ABAIXO PARA CRIAR NãoVAS QUESTOES */
-      QUESTAO_5_D:{
+      q_tast:{
         type:"radio", //NAO MUDE
-        title:"Diminuição ou perda do paladar ou olfato",
+        title:"O aluno/criança tem Diminuição ou perda do paladar ou olfato",
         subtitle:"Não relacionada a alergias sazonais, distúrbios neurológicos, ou outras causas ou problemas de saúde conhecidos preexistentes",
         qoptions:[ //NAO MUDE
           {text:"Sim", value:"yes"},
@@ -124,9 +111,9 @@ export default {
       /* O TEMPLATE TERMINA AQUI */
   
   /* COPIE E COLE O TEMPLATE ABAIXO PARA CRIAR NãoVAS QUESTOES */
-      QUESTAO_5_E:{
+      q_throat:{
         type:"radio", //NAO MUDE
-        title:"Dor de garganta ou dificuldade para engolir",
+        title:"O aluno/criança tem  dor de garganta ou dificuldade para engolir",
         subtitle:"Dor para engolir (não relacionada a alergias sazonais, refluxo ou outras causas ou problemas de saúde conhecidos preexistentes)",
         qoptions:[ //NAO MUDE
           {text:"Sim", value:"yes"},
@@ -137,9 +124,9 @@ export default {
   
   
   /* COPIE E COLE O TEMPLATE ABAIXO PARA CRIAR NãoVAS QUESTOES */
-      QUESTAO_5_F:{
+      q_nose:{
         type:"radio", //NAO MUDE
-        title:"Corrimento nasal ou constipação",
+        title:"O aluno/criança tem corrimento nasal ou constipação",
         subtitle:"Não relacionado a alergias sazonais, a estar ao tempo frio, ou outras causas ou problemas de saúde conhecidos preexistentes",
         qoptions:[ //NAO MUDE
           {text:"Sim", value:"yes"},
@@ -149,9 +136,9 @@ export default {
       /* O TEMPLATE TERMINA AQUI */
   
   /* COPIE E COLE O TEMPLATE ABAIXO PARA CRIAR NãoVAS QUESTOES */
-      QUESTAO_5_G:{
+      q_headache:{
         type:"radio", //NAO MUDE
-        title:"Dor de cabeça",
+        title:"O aluno/criança tem dor de cabeça",
         subtitle:"Atípica, duradoura (não relacionada a cefaleias de tensão, enxaqueca crónica, ou outras causas ou problemas conhecidos pre-existentes)",
         qoptions:[ //NAO MUDE
           {text:"Sim", value:"yes"},
@@ -161,9 +148,9 @@ export default {
       /* O TEMPLATE TERMINA AQUI */
   
   /* COPIE E COLE O TEMPLATE ABAIXO PARA CRIAR NãoVAS QUESTOES */
-      QUESTAO_5_H:{
+      q_vomit:{
         type:"radio", //NAO MUDE
-        title:"Náusea, vómito e/ou diarreia",
+        title:"O aluno/criança tem náusea, vómito e/ou diarreia",
         subtitle:"Não relacionados à síndrome do cólon irritável, ansiedade, cólicas menstruais, ou outras causas e problema de saúde conhecidos preexistentes",
         qoptions:[ //NAO MUDE
           {text:"Sim", value:"yes"},
@@ -174,9 +161,9 @@ export default {
   
   
   /* COPIE E COLE O TEMPLATE ABAIXO PARA CRIAR NãoVAS QUESTOES */
-      QUESTAO_5_I:{
+      q_tiredness:{
         type:"radio", //NAO MUDE
-        title:"Cansaço extremo ou dores musculares",
+        title:"O aluno/criança tem cansaço extremo ou dores musculares",
         subtitle:"Atípicos, fadiga, falta de energia, alimentação deficiente em bebés (não relacionado a depressão, insónia, disfunção tireoidiana, lesão súbita, ou outras causas ou problemas de saúde conhecidos preexistentes)",
         qoptions:[ //NAO MUDE
           {text:"Sim", value:"yes"},
@@ -187,7 +174,7 @@ export default {
   
    
   /* COPIE E COLE O TEMPLATE ABAIXO PARA CRIAR NãoVAS QUESTOES */
-      QUESTAO_6:{
+      q_relatives:{
         type:"radio", //NAO MUDE
         title:"Alguma pessoa com quem o aluno/criança vive apresenta atualmente algum sintoma novo de COVID-19 e/ou está a aguardar resultados após ter apresentado sintomas?",
         subtitle:"",
